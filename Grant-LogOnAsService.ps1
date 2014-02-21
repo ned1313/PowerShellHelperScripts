d8b2562df439960e692e18b35a9f559e2fb213fb
ned1313 <ned.bellavance@gmail.com>
2014-02-21 15:36:41 -0500

    Grant-LogOnAsService and First Sync

---
+++ Grant-LogOnAsService.ps1
@@ -0,0 +1,39 @@
+﻿function Grant-LogOnAsService{
+param(
+    [string[]] $users
+    )
+    
+
+    #Get list of currently used SIDs 
+    secedit /export /cfg tempexport.inf 
+    $curSIDs = Select-String .\tempexport.inf -Pattern "SeServiceLogonRight" 
+    $Sids = $curSIDs.line 
+    $sidstring = ""
+    foreach($user in $users){
+        $objUser = New-Object System.Security.Principal.NTAccount($user)
+        $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+        if($Sids -notcontains $strSID -and $sids -notcontains $user){
+            $sidstring += ",*$strSID"
+        }
+    }
+    if($sidstring){
+        $newSids = $sids + $sidstring
+        Write-Host "New Sids: $newSids"
+        $tempinf = Get-Content tempexport.inf
+        $tempinf = $tempinf.Replace($Sids,$newSids)
+        Add-Content -Path tempimport.inf -Value $tempinf
+    }
+    else{
+        Write-Host "No new sids"
+    }
+ 
+    secedit /import /db secedit.sdb /cfg ".\tempimport.inf" 
+    secedit /configure /db secedit.sdb 
+ 
+    gpupdate /force 
+ 
+    del ".\tempimport.inf" -force 
+    del ".\secedit.sdb" -force 
+    del ".\tempexport.inf" -force
+
+}
