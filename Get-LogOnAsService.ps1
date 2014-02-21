d17c0384bf0b8784cdc9397ff02ba0018554bf34
ned1313 <ned.bellavance@gmail.com>
2014-02-21 16:10:24 -0500

    More LogOnAsService scripts
    Adding Get and Test functions

---
+++ Get-LogOnAsService.ps1
@@ -0,0 +1,13 @@
+﻿function Get-LogOnAsService{
+    
+    #Get list of currently used SIDs 
+    secedit /export /cfg tempexport.inf 
+    $curSIDs = Select-String .\tempexport.inf -Pattern "SeServiceLogonRight" 
+    $Sids = $curSIDs.line 
+
+    del ".\secedit.sdb" -force 
+    del ".\tempexport.inf" -force
+
+    return $Sids
+
+}
---
+++ Test-LogOnAsService.ps1
@@ -0,0 +1,24 @@
+﻿function Test-LogOnAsService{
+param(
+    [string[]] $users
+    )
+    
+    $returnValue = $true
+    #Get list of currently used SIDs 
+    secedit /export /cfg tempexport.inf 
+    $curSIDs = Select-String .\tempexport.inf -Pattern "SeServiceLogonRight" 
+    $Sids = $curSIDs.line 
+    foreach($user in $users){
+        $objUser = New-Object System.Security.Principal.NTAccount($user)
+        $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+        if($Sids -notcontains $strSID -and $sids -notcontains $user){
+            $returnValue = $false
+        }
+    }
+ 
+    del ".\secedit.sdb" -force 
+    del ".\tempexport.inf" -force
+
+    return $returnValue
+
+}
